4c1aa1ed9c3ecf634daddeeb5de774595bf72c10
Winston Davis Mclean <winston.davis.mclean@gmail.com>
2016-08-09 16:01:16 -0400

    worked on css and user show page.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,7 +7,7 @@ class UserController < ApplicationController
   def create
     @user = User.create!(user_params)
     session[:user_id] = @user.id
-    redirect_to user_path(@user)
+    redirect_to user_path(@user.id)
   end
     
   def show
