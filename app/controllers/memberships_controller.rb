fe1b97d3309d61f313d512bdf789c9899227668d
Winston Davis Mclean <winston.davis.mclean@gmail.com>
2016-08-10 17:32:22 -0400

    validations seem to working

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -10,7 +10,19 @@ class MembershipsController < ApplicationController
     @current_user = User.find(session[:user_id])
     @competition = Competition.find(params[:competition_id])
     @membership = Membership.create!({user_id: @current_user.id, competition_id: @competition.id})
+    if @membership.save
+      flash[:success] = "#{@current_user.username} has joined #{@competition.title}"
+    end
     redirect_to competition_path(@competition)
+  end
+
+  def destroy
+    @current_user = User.find(session[:user_id])
+    @competition = Competition.find(params[:competition_id])
+    @membership = Membership.find({user_id: @current_user.id, competition_id: @competition.id})
+    @membership.destory
+    redirect_to user_path(@current_user)
 
   end
+    
 end
